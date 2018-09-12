75ef8db04cf646e510c82cafc7fccfa8de68298c
Mark Kuczmarski <markkuczmarski@gmail.com>
2018-09-12 11:24:35 +0900

    Fixed mv-postgresql gem

--- lib/mv/postgresql/railtie.rb
+++ lib/mv/postgresql/railtie.rb
@@ -2,8 +2,7 @@ module Mv
   module Postgresql
     class Railtie < ::Rails::Railtie
       initializer 'mv-postgresql.initialization', after: 'active_record.initialize_database' do
-        if defined?(::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter) &&
-           ::ActiveRecord::Base.connection.is_a?(::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
+        if defined?(::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter) && Rails.configuration.database_configuration[Rails.env]['adapter'] == 'postgresql'
           require 'mv/postgresql/loader.rb'
         end
       end
